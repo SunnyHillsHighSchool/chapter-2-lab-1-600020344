2adaf41257a2f448362470a91b1b698b8fd64b23
600020344 <77069279+600020344@users.noreply.github.com>
2021-03-08 21:01:47 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+class Main
+{
+	public static ArrayList<Integer> test()
+	{
+		//write code here
+    ArrayList<Integer> a = new ArrayList<Integer>();
+    a.add(1);
+		a.add(2);
+    a.add(3);
+    return a;
+	}
+  public static void main (String[] args)
+  {
+    System.out.println(test());
+  }
+}
